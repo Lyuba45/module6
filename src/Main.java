ca19dd88d68edfef39e1f15acbb6aa361ac0f864
Lyuba <kadge2@rambler.ru>
2022-02-08 23:12:09 +0300

    add Square class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Rectangle r = new Rectangle(4,5);
+        Rectangle r = new Rectangle(4,7);
         r.getArea();
         r.getPerimeter();
         Square s = new Square(6);
